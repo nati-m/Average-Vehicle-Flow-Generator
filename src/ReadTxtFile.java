6820c2d1dd021df215598fabc0d361146495984b
patri-m <patrick_6by20@hotmail.co.uk>
2020-05-12 15:04:50 +0100

    Added scanning function that gets time and vehicle numbers for every time in the text file, currently hardcoded to be only Tuesday-Thursday but that can be adjusted. The vehicle amounts are saved in an instance QuarterHourCollection, which contains all vehicle values from that time period mentioned in the text file (Tue-Thu) and can return them as an average

--- src/ReadTxtFile.java
+++ src/ReadTxtFile.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -14,7 +15,8 @@ public class ReadTxtFile {
 
 	public static void main(String[] args) {
 		Scanner scanMonth;
-		monthData = new File("C:/dev/HUDDERSFIELDTRAFFIC/WRAC1_C1_20_02.txt");
+		//monthData = new File("C:/dev/HUDDERSFIELDTRAFFIC/WRAC1_C1_20_02.txt");
+		monthData = new File("C:/dev/HUDDERSFIELDTRAFFIC/MonthTxts/WRAC1_D1_20_02.txt");
 		
 		try {
 			scanMonth = new Scanner(monthData);
@@ -29,15 +31,28 @@ public class ReadTxtFile {
 		initTimes();
 		
 		getTuesdayToThursdayData(scanMonth);
+		
+		printAverages();
+		
+		String[] trafficSensorNameNoColon = trafficSensorName.split(":");
+		String nameOfFile = trafficSensorNameNoColon[0] + trafficSensorNameNoColon[1] + "-AVERAGES.csv";
+		WriteFile.INSTANCE.writeToFile(nameOfFile, trafficSensorName, timesAndValues);
 
 	}
 	
+	private static void printAverages(){
+		for (Map.Entry<String, QuarterHourCollection> entry : timesAndValues.entrySet()) {
+		    System.out.println(entry.getKey()+" Vehicle Average: "+entry.getValue().findAverage());
+		}
+	}
+	
 	/*
-	 * This creates a HashMap of QuarterHourCollection objects, each for a fifteen minute time between 0.00am
-	 * and 23.45pm. This will be used in storing data
+	 * This creates a LinkedHashMap of QuarterHourCollection objects, each for a fifteen minute time between 0.00am
+	 * and 23.45pm. This will be used in storing data. LinkedHashMaps keep the order of pair insertion, so the times
+	 * will not get out of order as they would with a normal HashMap.
 	 */
 	private static void initTimes(){
-		timesAndValues = new HashMap<String, QuarterHourCollection>();
+		timesAndValues = new LinkedHashMap<String, QuarterHourCollection>();
 		
 		//There are 96 time values in all. Each is 15 minutes, or 25% (0.25) of an hour apart, starting at 0.
 		//This next for loop generates them all. It happens 96 times for this reason
--- src/WriteFile.java
+++ src/WriteFile.java
@@ -1,11 +1,73 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 public class WriteFile {
 	
 	protected static final WriteFile INSTANCE = new WriteFile();
 	
+	private PrintWriter writer;
+	private boolean printWriterError = false;
 	
 	private WriteFile(){}
 	
+	protected void writeToFile(String nameOfFile, String trafficSensorName, Map<String, QuarterHourCollection> timesAndValues){
+		
+		initPrintWriter(nameOfFile);
+		if(printWriterError){
+			return;
+		}
+		
+		writer.println("Traffic demands average Tuesday to Thursday for " + trafficSensorName + ",");
+		writer.println(",,");
+		
+		//now begin the main table
+		writer.println("Time, Average Vehicle Numbers Tue-Thu for " + trafficSensorName);
+		for (Map.Entry<String, QuarterHourCollection> entry : timesAndValues.entrySet()) {
+		    writer.println(entry.getValue().getTime() + "," + entry.getValue().findAverage());
+		}
+		
+		
+		
+		
+		writer.close();
+		
+	}
 	
+	protected void initPrintWriter(String fileName){
+		try {
+			writer = new PrintWriter(fileName, "UTF-8");
+		} catch (FileNotFoundException e) {
+			System.out.println("initPrintWriter error - file not found exception");
+			printWriterError = true;
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("initPrintWriter error - unsupported encoding exception");
+			printWriterError = true;
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private String getFormattedStringOfDouble(double value){
+		String toReturn = "" + value;
+		
+		int numberOfCharactersNeeded = 5;
+		if(toReturn.charAt(1) == '.'){ //if it's a single digit number we need one less character
+			numberOfCharactersNeeded = 4;
+		}
+		
+		for (int i = toReturn.length(); i < numberOfCharactersNeeded; i++){
+			toReturn = toReturn + "0";
+		}
+		
+		toReturn += ":00";
+		
+		toReturn = toReturn.replace('.', ':');
+		
+		
+		return toReturn;
+	}
 
 }
